d29ca7a78b63dbe8f5aec95b2270e35cbf6d6b49
mndgit <mndhome@outlook.com>
2020-06-17 14:49:43 -0400

    fix bug

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 // dropdown event
-function optionChanged(value) {
+function optionChanged(optionText) {
   d3.json("https://dataanalyticsproject.github.io/StockMarketAnalysis/Create%20APIs%20for%20Data/data.json")
   .then(function(data) {
 
@@ -23,60 +23,77 @@ function optionChanged(value) {
       openValue.push(ticker[i].Open);
       stockValue.push(ticker[i].Stock)
     }
-    
-    //create candlestick
-    var trace1 = {
-      
-      x: dateValue, 
-      close: closeValue,
-      decreasing: {line: {color: '#FF0000'}}, 
-      high: highValue, 
-      increasing: {line: {color: '##008000'}}, 
-      line: {color: 'rgba(31,119,180,1)'}, 
-      low: lowValue, 
-      open: openValue, 
-      
-      type: value, 
-      xaxis: 'x',   
-      yaxis: 'y'
-    };
 
-    var data = [trace1];
+    if (optionText == 'Candlestick') {
+      //create candlestick
+      var trace1 = {
+        
+        x: dateValue, 
+        close: closeValue,
+        decreasing: {line: {color: '#FF0000'}}, 
+        high: highValue, 
+        increasing: {line: {color: '##008000'}}, 
+        line: {color: 'rgba(31,119,180,1)'}, 
+        low: lowValue, 
+        open: openValue, 
+        
+        type: 'candlestick', 
+        xaxis: 'x',   
+        yaxis: 'y'
+      };
 
-    var layout = {
-      dragmode: 'zoom', 
-      margin: {
-        r: 10, 
-        t: 25, 
-        b: 40, 
-        l: 60
-      }, 
-      showlegend: false, 
-      xaxis: {
-        autorange: true, 
-        domain: [0, 1], 
-        range: ['2010-01-01', '2020-06-09'], 
-        rangeslider: {range: ['2010-01-01', '2020-06-09']}, 
-        type: 'date',
-        rangeslider: {
-          visible: false
+      var data = [trace1];
+
+      var layout = {
+        dragmode: 'zoom', 
+        margin: {
+          r: 10, 
+          t: 25, 
+          b: 40, 
+          l: 60
+        }, 
+        showlegend: false, 
+        xaxis: {
+          autorange: true, 
+          domain: [0, 1], 
+          range: ['2010-01-01', '2020-06-09'], 
+          rangeslider: {range: ['2010-01-01', '2020-06-09']}, 
+          type: 'date',
+          rangeslider: {
+            visible: false
+          }
+        }, 
+        yaxis: {
+          autorange: true, 
+          domain: [0, 1], 
+          type: 'linear'
         }
-      }, 
-      yaxis: {
-        autorange: true, 
-        domain: [0, 1], 
-        type: 'linear'
+      };
+
+      var config = {responsive: true}
+
+      Plotly.newPlot('plot', data, layout, config);
+    } else {
+        // create line graph
+        var trace1 = {
+          x: dateValue,
+          y: closeValue,
+          type: 'scatter'
+        };
+        
+        var data = [trace1];
+    
+        var layout = {};
+    
+        var config = {responsive: true}
+        
+        Plotly.newPlot('plot', data, layout, config);
       }
-    };
-
-    var config = {responsive: true}
-
-    Plotly.newPlot('plot', data, layout, config);
   });
 }
 
 function initializeData() {
-  optionChanged('candlestick');
+  optionChanged('Candlestick');
 }
 
 initializeData();
